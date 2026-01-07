dd41d0dad59aa89fd8d54f2d46708aacc9227891
Saad Ali <saadalileo456@gmail.com>
2026-01-08 02:14:22 +0500

    Add files via upload

---
+++ AdminPatientList.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Hospital_Management_System
+{
+    partial class AdminPatientList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminPatientList));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.search_txtbox = new System.Windows.Forms.TextBox();
+            this.search_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logout_btn = new System.Windows.Forms.Button();
+            this.patientsTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientsTableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(477, 215);
+            this.dataGridView1.TabIndex = 94;
+            // 
+            // back_btn
+            // 
+            this.back_btn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.back_btn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back_btn.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.back_btn.Location = new System.Drawing.Point(391, 443);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(102, 38);
+            this.back_btn.TabIndex = 98;
+            this.back_btn.Text = "BACK";
+            this.back_btn.UseVisualStyleBackColor = false;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // search_txtbox
+            // 
+            this.search_txtbox.BackColor = System.Drawing.Color.AliceBlue;
+            this.search_txtbox.Location = new System.Drawing.Point(143, 416);
+            this.search_txtbox.Name = "search_txtbox";
+            this.search_txtbox.Size = new System.Drawing.Size(206, 20);
+            this.search_txtbox.TabIndex = 97;
+            this.search_txtbox.TextChanged += new System.EventHandler(this.search_txtbox_TextChanged);
+            // 
+            // search_btn
+            // 
+            this.search_btn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.search_btn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_btn.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.search_btn.Location = new System.Drawing.Point(16, 411);
+            this.search_btn.Name = "search_btn";
+            this.search_btn.Size = new System.Drawing.Size(88, 27);
+            this.search_btn.TabIndex = 96;
+            this.search_btn.Text = "SEARCH";
+            this.search_btn.UseVisualStyleBackColor = false;
+            this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(75, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 16);
+            this.label1.TabIndex = 95;
+            this.label1.Text = "The List of All patients of the hospital is given below";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(179, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Search by Firstname";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // logout_btn
+            // 
+            this.logout_btn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.logout_btn.Font = new System.Drawing.Font("Calibri", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logout_btn.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.logout_btn.Location = new System.Drawing.Point(7, 467);
+            this.logout_btn.Name = "logout_btn";
+            this.logout_btn.Size = new System.Drawing.Size(57, 24);
+            this.logout_btn.TabIndex = 99;
+            this.logout_btn.Text = "LOG OUT";
+            this.logout_btn.UseVisualStyleBackColor = false;
+            this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
+            // 
+            // patientsTableBindingSource
+            // 
+            this.patientsTableBindingSource.DataMember = "PatientsTable";
+            // 
+            // AdminPatientList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(501, 509);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.search_txtbox);
+            this.Controls.Add(this.search_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.logout_btn);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdminPatientList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminPatientList";
+            this.Load += new System.EventHandler(this.AdminPatientList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientsTableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource patientsTableBindingSource;
+        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.TextBox search_txtbox;
+        private System.Windows.Forms.Button search_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button logout_btn;
+    }
+}
---
+++ DischargedPatients.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Hospital_Management_System
+{
+    partial class DischargedPatients
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DischargedPatients));
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.remove_btn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remove_btn.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.remove_btn.Location = new System.Drawing.Point(169, 211);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(130, 38);
+            this.remove_btn.TabIndex = 30;
+            this.remove_btn.Text = "Remove Patient";
+            this.remove_btn.UseVisualStyleBackColor = false;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click);
+            // 
+            // back_btn
+            // 
+            this.back_btn.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.back_btn.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back_btn.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.back_btn.Location = new System.Drawing.Point(336, 291);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(102, 38);
+            this.back_btn.TabIndex = 29;
+            this.back_btn.Text = "BACK";
+            this.back_btn.UseVisualStyleBackColor = false;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 24F);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(61, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 36);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Discharged Patient list";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSlateGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(425, 129);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // DischargedPatients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(451, 356);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DischargedPatients";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DischargedPatients";
+            this.Load += new System.EventHandler(this.DischargedPatients_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
